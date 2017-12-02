ab9ddb33a7626b361fce7cc4b78b1baf9bad7df5
Kev Comedia <kevcomedia@gmail.com>
2017-12-02 19:47:36 +0800

    Update search term on submit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import SearchResultList from './SearchResultList';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+    };
+    this.handleSearchTermSubmit = this.handleSearchTermSubmit.bind(this);
+  }
+
+  handleSearchTermSubmit(searchTerm) {
+    this.setState({searchTerm});
+  }
+
   render() {
     return (
       <div className="App">
-        <SearchBar />
+        <SearchBar onSearchTermSubmit={this.handleSearchTermSubmit} />
         <SearchResultList />
       </div>
     );
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSearchTermSubmit = this.handleSearchTermSubmit.bind(this);
+  }
+
+  handleSearchTermSubmit(event) {
+    event.preventDefault();
+    // I'm not aware of a more "React-y" way of getting the input value, if any.
+    this.props.onSearchTermSubmit(event.target[0].value);
+  }
+
   render() {
     return (
       <div className="SearchBar">
         <h1>Wikipedia Viewer</h1>
-        <form>
+        <form onSubmit={this.handleSearchTermSubmit}>
           <input type="text" />
           <button>Search</button>
           <button type="button">Random</button>
